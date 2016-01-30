ae8ff49df36e5e8ed835af71c401e6bc71dbabcd
Toby Samples <tsamples@psclistens.com>
2016-01-29 19:17:41 -0600

    Add parameter stuff

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var app = express();
+var url = require('url');
 
-
-app.get('/mynodeapp/hello', function(req, res) {
+app.get('/mynodeapp/:firstPart/:test', function(req, res) {
+	var testParm = req.params.test;
+	var firstPart = req.params.firstPart;
 	var arr = new Array();
 	var obj = null;
+
+	
+	var url_parts = url.parse(req.url, true);
+	var query = url_parts.query;
+
 	for (var i=0; i<5; i++) {
 		obj = new Object();
-		obj.header = "My Title " + i;
-		obj.paragraph = "My Paragraph " + i;
+		obj.header = firstPart + " My Title " + testParm + " " + i;
+		obj.paragraph = "My Paragraph " + query.id + " " + i;
 		arr.push(obj);
 	}
 	res.setHeader("Access-Control-Allow-Origin", "*");
